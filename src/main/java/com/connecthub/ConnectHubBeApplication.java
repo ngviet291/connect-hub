3d2a74f49df7eb7d7771a1528fc1f3a3fbb4ee33
ngviet291 <vietnguyen29105@gmail.com>
2026-06-14 23:42:36 +0700

    Create project

---
+++ src/main/java/com/connecthub/ConnectHubBeApplication.java
@@ -0,0 +1,14 @@
+package com.connecthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class ConnectHubBeApplication {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(ConnectHubBeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/connecthub/ConnectHubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connecthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectHubBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
